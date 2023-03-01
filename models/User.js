faf1d071e5c8cbb8baeea8b4174841952491bc81
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-01 12:26:23 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
+const sequelize = require('../index')
+const { DataType }  = require('sequelize');
 
+const User = sequeliza.define('User', {
+  name: Datatype.STRING,
+  email: Datatype.STRING
+})
+
+module.exports = User;
