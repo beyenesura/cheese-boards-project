d8d6ff0ca0a1a97d87a82c9b4934614efc42d7a6
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-01 12:19:00 -0500

    Update Board.js

--- models/Board.js
+++ models/Board.js
@@ -1,1 +1,9 @@
+const sequelize = require('../index')
+const { DataType }  = require('sequelize');
 
+const Board = sequeliza.define('Board', {
+  name: Datatype.STRING,
+  email: Datatype.STRING
+})
+
+module.exports = User
