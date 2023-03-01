aa327558331155450d99aac8415d8cecf5dcccc1
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-01 12:21:43 -0500

    Update Cheese.js

--- models/Cheese.js
+++ models/Cheese.js
@@ -1,1 +1,10 @@
+const sequelize = require('../index')
+const { DataType }  = require('sequelize');
 
+const Cheese = sequeliza.define('Cheese', {
+  type: Datatype.STRING,
+  description: Datatype.STRING,
+  rating: Datatype.NUM
+})
+
+module.exports = Cheese
