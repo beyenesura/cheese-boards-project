76cd90109b5f40c5355c18a42230aea0c0b68b64
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-01 12:40:55 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,9 @@
-
+const {Board} = require('./Board')
+const {Cheese} = require('./Cheese')
+Cheeses.schema.add({
+  Board: {
+    type: Schema.Types.ObjectId,
+    ref: 'Board'
+  }
+})
+module.exports = { Board, Cheese }
